c9b3324e456a10a6f975db9233666b393add676f
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:06:37 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var cars="Mercedes";
+var str=cars.replace("Mercedes","Bmw")
+console.log(str)
+
+//using i
+var car="Mercedes";
+var string=car.replace(/MERCEDES/i,"Porche")
+console.log(string)
+
+//using g
+var caaar="Mercedes";
+var stri=caaar.replace(/MERCEDES/g,"Lambo")
+console.log(stri)
